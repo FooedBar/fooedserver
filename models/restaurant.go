c2dd0c1d76a8e25463b68c5240afff821f543ba0
Arthelon <hsing.daniel@gmail.com>
2016-05-21 15:39:36 +0800

    Added image fields to restaurant model

--- models/restaurant.go
+++ models/restaurant.go
@@ -6,6 +6,9 @@ type Restaurant struct {
 	Id          int64
 	Lat         float64
 	Long        float64
+	ImageUrl    string
+	ImageHeight int64
+	ImageWidth  int64
 	Name        string
 	Description string
 	Style       string
