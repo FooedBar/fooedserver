5956107a281723946853793bea58fff42e8daac0
Sasha Varlamov <sasha@sashavarlamov.com>
2016-05-21 16:17:29 +0800

    Add restaurant JSON

--- models/restaurant.go
+++ models/restaurant.go
@@ -3,12 +3,12 @@ package models
 import "time"
 
 type Restaurant struct {
-	Id          int64
-	Lat         float64
-	Long        float64
-	Name        string
-	Description string
-	Style       string
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	Id          int64     `json:"id"`
+	Lat         float64   `json:"lat"`
+	Long        float64   `json:"long"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Style       string    `json:"style"`
+	CreatedAt   time.Time `json:"-"`
+	UpdatedAt   time.Time `json:"-"`
 }
