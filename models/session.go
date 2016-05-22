80fc30d2a0bb1469f60f3f4453c31f26a7c6093d
Sasha Varlamov <sasha@sashavarlamov.com>
2016-05-22 08:40:46 +0800

    Add selection like status

--- models/session.go
+++ models/session.go
@@ -29,13 +29,13 @@ func (session *Session) Create() error {
 
 func (session *Session) GetSelectedMenuItems() ([]MenuItem, error) {
 	var selections []MenuItem
-	err := db.Raw("SELECT menu_item.* FROM selection, menu_item WHERE selection.session_id = " + fmt.Sprint(session.Id) + " AND menu_item.id = selection.menu_item_id").Scan(&selections).Error
+	err := db.Raw("SELECT menu_item.* FROM selection, menu_item WHERE selection.session_id = " + fmt.Sprint(session.Id) + " AND menu_item.id = selection.menu_item_id AND selection.like = 1").Scan(&selections).Error
 	return selections, err
 }
 
 func (session *Session) GetDetailedSelectedMenuItems() ([]DetailedMenuItem, error) {
 	var selections []DetailedMenuItem
-	err := db.Raw("SELECT menu_item.* FROM selection, menu_item WHERE selection.session_id = " + fmt.Sprint(session.Id) + " AND menu_item.id = selection.menu_item_id").Scan(&selections).Error
+	err := db.Raw("SELECT menu_item.* FROM selection, menu_item WHERE selection.session_id = " + fmt.Sprint(session.Id) + " AND menu_item.id = selection.menu_item_id AND selection.like = 1").Scan(&selections).Error
 	return selections, err
 }
 
